9502675fd879042d561aec99c54d402aef62ab04
Fred Zugs <41949716+Segebari@users.noreply.github.com>
2024-07-10 07:41:54 +0100

    adhd

--- src/pages/homepage/Home.jsx
+++ src/pages/homepage/Home.jsx
@@ -31,7 +31,7 @@ const Home = () => {
           <div className="w-full h-full flex items-center" id="welcome">
             <div className="w-full">
               <div className="text-center px-4 sm:px-6 lg:px-8">
-                <h1 className="text-5xl lg:text-7xl font-black pb-3 sm:pb-5 transition-all duration-500 ease-in-out text-white">
+                <h1 className="text-4xl md:text-5xl lg:text-7xl font-black pb-3 sm:pb-5 transition-all duration-500 ease-in-out text-white">
                   Elevate Your Lifestyle. <br /> Luxury Redefined.
                 </h1>
                 <p className="w-full max-w-3xl mx-auto text-sm sm:text-base lg:text-lg text-white">
